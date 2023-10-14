c8218b5384c93622fad26f3ed87fd1f2507792b3
Sunyb01 <18911031529@189.cn>
2023-10-14 22:00:21 +0800

    feat(*): 8.10一半

--- chapter8/abstract_base_class.py
+++ chapter8/abstract_base_class.py
@@ -1,6 +1,13 @@
 # 抽象基类
-from collections.abc import Mapping
+from collections.abc import Mapping, Sequence, Iterable as ITE, Hashable
+from collections import Counter
+from decimal import Decimal
+from random import shuffle
 from typing import Iterable
+# 显示使用TypeAlias
+from typing import TypeAlias, TypeVar
+
+FromTo: TypeAlias = tuple[str, str]
 
 # 发送时要保守, 接收时要大方;
 """
@@ -9,6 +16,8 @@ from typing import Iterable
     UserDict和dict是同级关系, 都是abc.MutableMapping的子类;
     一般来说在参数的类型提示中最好使用abc.Mapping或abc.MutableMapping
 """
+
+
 def name2hex(name: str, color_map: Mapping[str, int]) -> str:
     pass
 
@@ -16,16 +25,57 @@ def name2hex(name: str, color_map: Mapping[str, int]) -> str:
 def name2hex2(name: str, color_map: dict[str, int]) -> str:
     pass
 
+
 # Iterable 可迭代对象
-def zip_replace(text: str, changes: Iterable[tuple[str, str]]) -> str:
+def zip_replace(text: str, changes: Iterable[FromTo]) -> str:
     for from_, to in changes:
         text = text.replace(from_, to)
     return text
+
 
 def use_iterable():
     l33t = [('a', '4'), ('e', '3'), ('i', '1'), ('o', '0'), ]
     text = 'mad skilled noob powned leet'
     print(zip_replace(text, l33t))
+
+
+# 参数化泛型和TypeVar
+T = TypeVar('T')
+
+
+def sample(population: Sequence[T], size: int) -> list[T]:
+    if size < 1:
+        raise ValueError('size must be >= 1')
+    result = list(population)
+    shuffle(result)
+    return result[:size]
+
+
+# 1. 受限的TypeVar
+# 第一个参数为类型参数的名称, 类似于Java中的T,E等泛型词. 后面为其支持的范围;
+# 写法与Golang中的相似, Go中的泛型为VarName[T type1 | type2 | type3, E type4]
+NumberT = TypeVar('NumberT', float, int, Decimal)
+
+
+def mode(data: ITE[NumberT]) -> NumberT:
+    pass
+
+
+# 有界的TypeVar
+# 第一个参数为类型参数的名称, 类似于Java中的T,E等泛型词
+# 第二个参数为其上边界, 此处为Hashable或其子类;
+# 类似于Java中的<T extend Type_Class>
+HashableT = TypeVar('HashableT', bound=Hashable)
+
+
+def mode2(data: ITE[HashableT]) -> HashableT:
+    pass
+
+
+# 预定义的类型变量AnyStr
+# type模块提供了一个预定义的类型变量, AnyStr;
+# 接收btyes或str, 返回值也是这两个中的一个;
+
 
 if __name__ == '__main__':
     use_iterable()
---
+++ chapter8/static_protocol.py
@@ -0,0 +1,33 @@
+# 静态协议
+# Protocol类型类似于Go中的接口, 定义协议类型时指定一个活多个方法,
+# 在需要使用协议类型的地方,类型检查工具会核查有没有实现指定的方法;
+# 与Go一样, 与接口不需要建立任何关系, 如显示的继承与注册;
+
+from typing import Protocol, TypeVar
+from collections.abc import Iterable
+
+# 这相当于是Any, 前提是这个Any必须是可排序的
+T = TypeVar('T')
+
+
+def top_n(series: Iterable[T], length: int) -> list[T]:
+    ordered = sorted(series, reverse=True)
+    return ordered[:length]
+
+
+def non_sort_key():
+    objs = [object() for _ in range(4)]
+    try:
+        top_n(objs, 2)
+    except TypeError as te:
+        print('Has Error, ErrorMsg is ', te)
+
+
+if __name__ == '__main__':
+    # numbers = [4, 1, 5, 2, 6, 7, 3, ]
+    # print(top_n(numbers, 3))
+    # fruits = 'mango pear apple kiwi banana'.split()
+    # print(top_n(fruits, 3))
+    # fruits2 = [(len(s), s) for s in fruits]
+    # print(top_n(fruits2, 3))
+    non_sort_key()
